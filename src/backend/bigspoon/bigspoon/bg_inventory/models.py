632c6ba4f787071f5a391d14bcfaa2493f08f6e1
wgx731 <wgx731@gmail.com>
2013-10-13 14:19:11 +0800

    model init setup

--- src/backend/bigspoon/bigspoon/bg_inventory/models.py
+++ src/backend/bigspoon/bigspoon/bg_inventory/models.py
@@ -13,35 +13,7 @@ def _image_upload_path(instance, filename):
     return instance.get_upload_path(filename)
 
 
-def _thumbnail_sizes(instance):
-    return instance.get_thumbnail_size()
-
-
-def _image_helptext(instance):
-    return instance.get_image_helptext()
-
-
 # model class
-class BasicImage(models.Model):
-    """
-    Basic Image Model for store images
-    """
-    caption = models.CharField(
-        _('caption'),
-        max_length=300,
-        blank=True,
-        help_text=_('image caption')
-    )
-    image = ImageWithThumbsField(
-        upload_to=_image_upload_path,
-        sizes=_thumbnail_sizes,
-        help_text=_image_helptext
-    )
-
-    def __unicode__(self):
-        return "%s - %s" % (self.caption, self.image)
-
-
 class User(AbstractBaseUser, PermissionsMixin):
     """
     Custom User model, extending Django's AbstractBaseUser
@@ -114,22 +86,6 @@ class User(AbstractBaseUser, PermissionsMixin):
         verbose_name_plural = _('users')
 
 
-class RestaurantIcon(BasicImage):
-    class Meta:
-        proxy = True
-
-    def get_upload_path(self, filename):
-        fname, dot, extension = filename.rpartition('.')
-        slug = slugify(fname)
-        return '%s.%s' % (slug, extension)
-
-    def get_thumbnail_size():
-        return ((200, 200),)
-
-    def get_image_helptext():
-        return _('restaurant icon')
-
-
 class Restaurant(models.Model):
     """
     Stores restaurant information
@@ -140,20 +96,187 @@ class Restaurant(models.Model):
         unique=True,
         help_text=_('restaurant name')
     )
-    desc = models.TextField(
-        _('description'),
-        blank=False,
-        null=True,
-        help_text=_('restaurant description')
+    icon = ImageWithThumbsField(
+        upload_to=_image_upload_path,
+        sizes=((200, 200),),
+        help_text=_('restaurant icon')
     )
-    icon = models.ForeignKey(RestaurantIcon)
+
+    def get_upload_path(self, filename):
+        fname, dot, extension = filename.rpartition('.')
+        slug = slugify(self.name)
+        return 'restaurant/icons/%s.%s' % (slug, extension)
 
     def __unicode__(self):
         """
         Returns the restaurant name
         """
-        return slugify(self.name)
+        return self.name
 
     class Meta:
         verbose_name = _('restaurant')
         verbose_name_plural = _('restaurants')
+
+
+class Outlet(models.Model):
+    """
+    Stores outlet information
+    """
+    restaurant = models.ForeignKey(Restaurant)
+    name = models.CharField(
+        _('name'),
+        max_length=128,
+        unique=True,
+        help_text=_('outlet name')
+    )
+    desc = models.TextField(
+        _('description'),
+        blank=False,
+        null=True,
+        help_text=_('outlet description')
+    )
+
+    def __unicode__(self):
+        """
+        Returns the outlet name
+        """
+        return self.name
+
+    class Meta:
+        verbose_name = _('outlet')
+        verbose_name_plural = _('outlets')
+
+
+class Table(models.Model):
+    """
+    Stores outlet table information
+    """
+    outlet = models.ForeignKey(Outlet)
+    name = models.CharField(
+        _('name'),
+        max_length=128,
+        unique=True,
+        help_text=_('table name')
+    )
+    qrcode = models.CharField(
+        _('qrcode'),
+        max_length=255,
+        help_text=_('table qrcode identifier')
+    )
+
+    def __unicode__(self):
+        """
+        Returns the table name
+        """
+        return self.name
+
+    class Meta:
+        verbose_name = _('table')
+        verbose_name_plural = _('tables')
+
+
+class Dish(models.Model):
+    """
+    Stores outlet dish information
+    """
+    outlet = models.ForeignKey(Outlet)
+    name = models.CharField(
+        _('name'),
+        max_length=128,
+        unique=True,
+        help_text=_('outlet dish name')
+    )
+    desc = models.TextField(
+        _('description'),
+        blank=False,
+        null=True,
+        help_text=_('outlet dish description')
+    )
+    start_time = models.DateTimeField(
+        _('start time'),
+        blank=False,
+        help_text=_('dish start serving time'),
+    )
+    end_time = models.DateTimeField(
+        _('start time'),
+        blank=False,
+        help_text=_('dish end serving time'),
+    )
+    price = models.FloatField()
+    category = models.ManyToManyField()
+    photo = ImageWithThumbsField(
+        upload_to=_image_upload_path,
+        sizes=((640, 400),),
+        help_text=_('restaurant icon')
+    )
+
+    def get_upload_path(self, filename):
+        fname, dot, end = filename.rpartition('.')
+        slug = slugify(self.name)
+        return 'restaurant/dishes/%s/%s.%s' % (self.outlet.name, slug, end)
+
+    def __unicode__(self):
+        """
+        Returns the table name
+        """
+        return self.name
+
+    class Meta:
+        verbose_name = _('dish')
+        verbose_name_plural = _('dishes')
+
+
+class Rating(models.Model):
+    """
+    Stores dish rating information
+    """
+    dish = models.ForeignKey(Dish)
+    score = models.FloatField()
+
+    def __unicode__(self):
+        """
+        Returns the dish name
+        """
+        return self.dish.name
+
+    class Meta:
+        verbose_name = _('rating')
+        verbose_name_plural = _('ratings')
+
+
+class Review(models.Model):
+    """
+    Stores dish rating information
+    """
+    outlet = models.ForeignKey(Outlet)
+    score = models.FloatField()
+    review = models.TextField()
+
+    def __unicode__(self):
+        """
+        Returns the outlet name
+        """
+        return self.outlet.name
+
+    class Meta:
+        verbose_name = _('review')
+        verbose_name_plural = _('reviews')
+
+
+class Note(models.Model):
+    """
+    Stores dish rating information
+    """
+    user = models.ForeignKey(User)
+    outlet = models.ForeignKey(Outlet)
+    note = models.TextField()
+
+    def __unicode__(self):
+        """
+        Returns the outlet name and user name
+        """
+        return self.outlet.name + self.user.name
+
+    class Meta:
+        verbose_name = _('note')
+        verbose_name_plural = _('notes')
