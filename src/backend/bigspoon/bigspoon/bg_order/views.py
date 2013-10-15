29a057032983fcd9abc442d1dd36597993e39457
Yos Riady <yosriady@gmail.com>
2013-10-15 15:18:17 +0800

    Lint with flake8

--- src/backend/bigspoon/bigspoon/bg_order/views.py
+++ src/backend/bigspoon/bigspoon/bg_order/views.py
@@ -1,27 +1,29 @@
 from django.views.generic import TemplateView, ListView, FormView
 
+
 class StaffLoginView(FormView):
     pass
+
 
 class MainView(TemplateView):
     template_name = "bg_order/main.html"
 
-# class MainView(ListView):
-#     model = Order
-#     template_name = "bg_order/main.html"
-#     context_object_name = 'order'
 
 class MenuView(TemplateView):
     template_name = "bg_order/menu.html"
 
+
 class TableView(TemplateView):
     template_name = "bg_order/tables.html"
+
 
 class UserView(TemplateView):
     template_name = "bg_order/user.html"
 
+
 class HistoryView(TemplateView):
     template_name = "bg_order/history.html"
+
 
 class ReportView(TemplateView):
     template_name = "bg_order/report.html"
