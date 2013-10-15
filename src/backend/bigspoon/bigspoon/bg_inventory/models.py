22a18b6ba8423215b83f596beeca02c32da78a3d
wgx731 <wgx731@gmail.com>
2013-10-15 09:22:06 +0800

    small change in models

--- src/backend/bigspoon/bigspoon/bg_inventory/models.py
+++ src/backend/bigspoon/bigspoon/bg_inventory/models.py
@@ -30,9 +30,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(
         _('username'),
         max_length=30,
-        db_index=True,
-        blank=False,
-        null=False,
+        blank=True,
         help_text=_('username for internal use'),
     )
     first_name = models.CharField(
@@ -64,7 +62,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     )
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
 
     objects = UserManager()
 
@@ -80,6 +77,8 @@ class User(AbstractBaseUser, PermissionsMixin):
         """
         Returns the user email
         """
+        if self.username:
+            return self.username
         return self.email
 
     def __unicode__(self):
@@ -173,9 +172,9 @@ class Table(models.Model):
 
     def __unicode__(self):
         """
-        Returns the table name
+        Returns the outlet and table name
         """
-        return self.name
+        return '%s - %s' % (self.outlet.name, self.name)
 
     class Meta:
         verbose_name = _('table')
@@ -225,7 +224,7 @@ class Dish(models.Model):
         max_length=255,
         blank=False,
         null=True,
-        help_text=_('outlet dish pos id')
+        help_text=_('outlet pos system dish id')
     )
     desc = models.TextField(
         _('description'),
@@ -246,7 +245,7 @@ class Dish(models.Model):
         help_text=_('dish end time'),
     )
     price = models.DecimalField(
-        max_digits=10,
+        max_digits=6,
         decimal_places=2,
     )
     photo = ImageWithThumbsField(
@@ -276,6 +275,7 @@ class Rating(models.Model):
     """
     Stores dish rating information
     """
+    user = models.ForeignKey(User)
     dish = models.ForeignKey(Dish)
     score = models.DecimalField(
         max_digits=2,
@@ -284,9 +284,9 @@ class Rating(models.Model):
 
     def __unicode__(self):
         """
-        Returns the dish name and score
+        Returns the user and dish name
         """
-        return "%s - %d" % (self.dish.name, self.score)
+        return "%s - %s" % (self.user.email, self.dish.name)
 
     class Meta:
         verbose_name = _('rating')
@@ -297,6 +297,7 @@ class Review(models.Model):
     """
     Stores dish rating information
     """
+    user = models.ForeignKey(User)
     outlet = models.ForeignKey(Outlet)
     score = models.DecimalField(
         max_digits=2,
@@ -311,9 +312,9 @@ class Review(models.Model):
 
     def __unicode__(self):
         """
-        Returns the outlet name and score
+        Returns the user and outlet name
         """
-        return "%s - %d" % (self.outlet.name, self.score)
+        return "%s - %s" % (self.user.email, self.outlet.name)
 
     class Meta:
         verbose_name = _('review')
@@ -330,7 +331,7 @@ class Note(models.Model):
         _('note'),
         blank=False,
         null=True,
-        help_text=_('note for user from outlet manager')
+        help_text=_('outlet note for user')
     )
 
     def __unicode__(self):
