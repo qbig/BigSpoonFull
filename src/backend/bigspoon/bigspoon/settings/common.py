f716d1e0b946c9f1ef74dd0078647c347c63adbe
wgx731 <wgx731@gmail.com>
2013-10-15 11:39:06 +0800

    Add authtoken

--- src/backend/bigspoon/bigspoon/settings/common.py
+++ src/backend/bigspoon/bigspoon/settings/common.py
@@ -202,6 +202,9 @@ THIRD_PARTY_APPS = (
     # Django rest framework:
     'rest_framework',
 
+    # Django rest framework authtoken:
+    'rest_framework.authtoken',
+
     # Django object level permission:
     'guardian',
 
