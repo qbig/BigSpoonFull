79451150d12b237a797f2ff76c0416619e4b2997
Yos Riady <yosriady@gmail.com>
2013-10-15 14:30:33 +0800

    Admin additions

--- src/backend/bigspoon/bigspoon/bg_order/admin.py
+++ src/backend/bigspoon/bigspoon/bg_order/admin.py
@@ -5,6 +5,7 @@ from bg_order.models import Request, Meal, Order
 class RequestAdmin(admin.ModelAdmin):
     list_display = ['diner', 'table', 'request_type', 'is_active', 'created', 'finished']
     list_display_links = ('diner', 'table',)
+    search_fields = ['diner__first_name', 'diner__last_name']
 
 class MealAdmin(admin.ModelAdmin):
     list_display = ['diner', 'table', 'is_active', 'is_paid']
@@ -13,13 +14,11 @@ class MealAdmin(admin.ModelAdmin):
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'meal', 'get_diner', 'dish',
                     'quantity', 'created', 'modified', 'bill_time']
-
     list_display_links = ('meal','dish', 'get_diner')
 
     def get_diner(self, obj):
             return '%s'%(obj.meal.diner)
     get_diner.short_description = 'Diner'
-    search_fields = ['meal_id']
 
 admin.site.register(Request, RequestAdmin)
 admin.site.register(Meal, MealAdmin)
