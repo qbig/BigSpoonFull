3e24d450a41d5fc8a646fae64b7326475ae4de5f
wgx731 <wgx731@gmail.com>
2013-10-17 00:24:51 +0800

    Fix admin user issue

--- src/backend/bigspoon/bigspoon/bg_inventory/admin.py
+++ src/backend/bigspoon/bigspoon/bg_inventory/admin.py
@@ -2,14 +2,26 @@ from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth import get_user_model
 from guardian.admin import GuardedModelAdmin
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 
 User = get_user_model()
 
 from bg_inventory.models import Restaurant, Outlet, Table,\
     Category, Dish, Rating, Review, Note
+from bg_inventory.forms import BGUserCreationForm
 
 
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(AuthUserAdmin):
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 'username', 'password1', 'password2')
+        }),
+    )
+    add_form = BGUserCreationForm
+
     fieldsets = (
         (None, {'fields': ('email', 'username', 'password')}),
         (_('Personal info'), {'fields': (
@@ -19,7 +31,10 @@ class UserAdmin(admin.ModelAdmin):
         (_('Important dates'), {'fields': (
             'last_login', 'date_joined')}),
     )
+    list_display = ('email', 'username', 'is_staff', 'is_superuser',
+                    'is_active')
     search_fields = ['email', 'username', 'first_name', 'last_name']
+    readonly_fields = ['last_login', 'date_joined']
 
 
 """
@@ -29,9 +44,9 @@ Restaurant -> Outlet
 Outlet -> Table, Review, Note
 """
 
+
 class RestaurantAdmin(GuardedModelAdmin):
     search_fields = ['name']
-    pass
 
 
 class OutletAdmin(GuardedModelAdmin):
@@ -46,27 +61,26 @@ class TableAdmin(GuardedModelAdmin):
 
 class CategoryAdmin(GuardedModelAdmin):
     search_fields = ['name', 'desc']
-    pass
 
 
 class DishAdmin(GuardedModelAdmin):
     raw_id_fields = ('outlet', 'categories')
-    search_fields = ['name', 'desc', 'pos', 'price'] #outlet, categories
+    search_fields = ['name', 'desc', 'pos', 'price']
 
 
 class RatingAdmin(GuardedModelAdmin):
     raw_id_fields = ('dish',)
-    search_fields = ['score'] #user, dish
+    search_fields = ['score']
 
 
 class ReviewAdmin(GuardedModelAdmin):
     raw_id_fields = ('outlet',)
-    search_fields = ['score', 'feedback'] #user, outlet
+    search_fields = ['score', 'feedback']
 
 
 class NoteAdmin(GuardedModelAdmin):
     raw_id_fields = ('outlet', 'user')
-    search_fields = ['note'] #user, outlet
+    search_fields = ['note']
 
 
 admin.site.register(User, UserAdmin)
