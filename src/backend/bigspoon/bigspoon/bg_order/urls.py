ce5d5ce6fbc68c7bf4853681e04eb9c71aea9eb1
wgx731 <wgx731@gmail.com>
2013-10-21 17:55:23 +0800

    Add login required for staff pages

--- src/backend/bigspoon/bigspoon/bg_order/urls.py
+++ src/backend/bigspoon/bigspoon/bg_order/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
 from bg_order.views import MainView, MenuView, TableView, UserView,\
     HistoryView, ReportView, MenuAddView
 
 urlpatterns = patterns('',
-    url(r'^main/$', MainView.as_view(), name='staff_main'),
-    url(r'^menu/$', MenuView.as_view(), name='staff_menu'),
-    url(r'^menu/add/$', MenuAddView.as_view(), name='staff_menu_add'),
-    url(r'^tables/$', TableView.as_view(), name='staff_table'),
-    url(r'^user/$', UserView.as_view(), name='staff_user'),
-    url(r'^history/$', HistoryView.as_view(), name='staff_history'),
-    url(r'^report/$', ReportView.as_view(), name='staff_report'),
+    url(r'^main/$', login_required(MainView.as_view()), name='staff_main'),
+    url(r'^menu/$', login_required(MenuView.as_view()), name='staff_menu'),
+    url(r'^menu/add/$', login_required(MenuAddView.as_view()), name='staff_menu_add'),
+    url(r'^tables/$', login_required(TableView.as_view()), name='staff_table'),
+    url(r'^user/$', login_required(UserView.as_view()), name='staff_user'),
+    url(r'^history/$', login_required(HistoryView.as_view()), name='staff_history'),
+    url(r'^report/$', login_required(ReportView.as_view()), name='staff_report'),
 )
