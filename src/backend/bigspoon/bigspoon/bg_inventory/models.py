77f040e38c1168f1b8aa1a6e47988155a5192142
wgx731 <wgx731@gmail.com>
2013-10-22 20:57:19 +0800

    change user full name method

--- src/backend/bigspoon/bigspoon/bg_inventory/models.py
+++ src/backend/bigspoon/bigspoon/bg_inventory/models.py
@@ -70,7 +70,10 @@ class User(AbstractBaseUser, PermissionsMixin):
         """
         Returns the first_name plus the last_name, with a space in between
         """
-        full_name = '%s %s' % (self.first_name, self.last_name)
+        if self.first_name and self.last_name:
+            full_name = '%s %s' % (self.first_name, self.last_name)
+        else:
+            full_name = self.email
         return full_name.strip()
 
     def get_short_name(self):
