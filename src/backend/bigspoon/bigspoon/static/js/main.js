e34d1a7ce6431e8468243b544c25401bf26500b7
wgx731 <wgx731@gmail.com>
2013-10-28 17:46:36 +0800

    restrcture js

--- src/backend/bigspoon/bigspoon/static/js/main.js
+++ src/backend/bigspoon/bigspoon/static/js/main.js
@@ -1,9 +1,8 @@
-var container = document.querySelector('#main');
+$(document).ready(function() {
 
-var msnry = new Masonry( container, {
-  // options
-  itemSelector: '.item'
+    var container = document.querySelector('#main');
+    var msnry = new Masonry( container, {
+        // options
+        itemSelector: '.item'
+    });
 });
-
-$('#id_start_time').timepicker();
-$('#id_end_time').timepicker();
