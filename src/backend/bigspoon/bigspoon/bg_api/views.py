e213c65aadae64fc091e88a2168d6fbae00615fd
cl <cl@startup.org.sg>
2013-10-31 21:36:27 +0800

    a more user-friendly out of stock message.

--- src/backend/bigspoon/bigspoon/bg_api/views.py
+++ src/backend/bigspoon/bigspoon/bg_api/views.py
@@ -172,8 +172,10 @@ class CreateMeal(generics.CreateAPIView):
                 out_of_stock.append(dish.name)
 
         if(len(out_of_stock) > 0):
+            out_of_stock_str = ", ".join(out_of_stock[:-1]) #exclude last item, for "and" concatenation below.
+            out_of_stock_str += " and "+out_of_stock[-1] if len(out_of_stock)>1
             return Response({"error": "Sorry, we ran out of stock for "
-                             + ", ".join(out_of_stock)},
+                             + out_of_stock_str},
                             status=status.HTTP_400_BAD_REQUEST)
 
         diner = request.user
