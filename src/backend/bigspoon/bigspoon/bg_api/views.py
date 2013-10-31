9ef42bb4ad7a8f9bc04ca572750feb1d5ddb09db
cl <cl@startup.org.sg>
2013-10-31 21:38:51 +0800

    typo

--- src/backend/bigspoon/bigspoon/bg_api/views.py
+++ src/backend/bigspoon/bigspoon/bg_api/views.py
@@ -173,7 +173,8 @@ class CreateMeal(generics.CreateAPIView):
 
         if(len(out_of_stock) > 0):
             out_of_stock_str = ", ".join(out_of_stock[:-1]) #exclude last item, for "and" concatenation below.
-            out_of_stock_str += " and "+out_of_stock[-1] if len(out_of_stock)>1
+            if (len(out_of_stock)>1):
+                out_of_stock_str += " and "+out_of_stock[-1]
             return Response({"error": "Sorry, we ran out of stock for "
                              + out_of_stock_str},
                             status=status.HTTP_400_BAD_REQUEST)
