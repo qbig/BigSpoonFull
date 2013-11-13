a321763e594c1775d5cc95eb8421b2ab8225445e
cl <cl@startup.org.sg>
2013-11-13 21:31:25 +0800

    week calculation

--- src/backend/bigspoon/bigspoon/assets/js/chart/chartmanager.js
+++ src/backend/bigspoon/bigspoon/assets/js/chart/chartmanager.js
@@ -29,14 +29,114 @@ function getMonthStr(monthNum){
 	}
 }
 
-function plotChart(){
+createChart();
+function createChart(){
 	var chartSpending = document.getElementById("chart-spending");
 	var chartTOTime = document.getElementById("chart-totime");
 
-	var chartDates = getChartDates("weeks");
+	var chartDates = getChartDates("week");
 	var datePeriods = chartDates.periods;
 	var chartLabels = chartDates.labels;
 	var chartData = [];
+	var lineChartData = {
+		labels : chartLabels,
+		datasets : [
+			{
+				fillColor : "rgba(220,220,220,0.5)",
+				strokeColor : "rgba(220,220,220,1)",
+				pointColor : "rgba(220,220,220,1)",
+				pointStrokeColor : "#fff",
+				data : chartData,
+			}
+		]
+	};
+
+
+var testData = [1,2,3,65,59,90,81,56,55,40];
+datePeriods = testData;
+	for (var i=0; i<datePeriods.length; ++i){
+		//var getData = $.post("python", );
+		chartData.push(testData[i]);
+		updateChart(chartSpending, lineChartData);
+	}
+	//var myLine = new Chart(chartSpending.getContext("2d")).Line(lineChartData);
+
+}
+function getChartDates(periodInterval){
+  switch (true){
+  case /^month$/.test(periodInterval):
+    return getChartMonthLabels(); break;
+  case /^week$/.test(periodInterval):
+    return getChartWeekLabels(); break;
+  }
+}
+function updateChart(chartCanvas, lineChartData){
+	var myLine = new Chart(chartCanvas.getContext("2d")).Line(lineChartData);
+}
+
+
+
+function getChartWeekLabels(){ //function weekLabels(){ // weekLabels(currTimeObj, currWeek)
+	var currTimeObj = new Time();
+	var numWeeksThisMonth = currTimeObj.weeksInMonth();
+	var currWeek = currTimeObj.weekOfCurrentMonth();
+
+	var YMD = getTimeYMD(currTimeObj);
+	var focusDate = new Time(YMD.y, YMD.m, YMD.d);
+
+	var numWeeks = 0;
+	var firstDisplayWeekNum = 0;
+	var firstDisplayWeek = null;
+	//If not the last week of the month, need to show weeks from previous month.
+	if (currWeek<numWeeksThisMonth){
+		focusDate.advanceMonths(-1);
+		//c!onsole.log(focusDate);
+		//Don't get .weeksInMonth() from .advanceMonths(-1).beginningOfWeek()
+		//Bad example: Today is Friday, 1 Nov. One month back is Tuesday, 1 Oct.
+		//First day of week would be Monday, 30 Sept. Number of weeks in that first day, would belong to Sept, not Oct.
+		numWeeks = focusDate.weeksInMonth(); //numWeeksLastMonth
+		firstDisplayWeekNum = focusDate.weekOfCurrentMonth();
+		focusDate.beginningOfWeek();
+		firstDisplayWeek = focusDate;
+	}
+	else {
+		focusDate.beginningOfMonth();
+		numWeeks = numWeeksThisMonth;
+		firstDisplayWeekNum = focusDate.weekOfCurrentMonth();
+		focusDate.beginningOfWeek();
+		firstDisplayWeek = focusDate;
+	}
+
+	var labels = [];
+	var periods = [];
+	for(var i=0; i<numWeeks; ++i){
+		var weeknum = (firstDisplayWeekNum+i)%numWeeks;
+		weeknum = (weeknum==0)? numWeeks : weeknum;
+		var currYMD = getTimeYMD(focusDate);
+		labels.push("Week of"+": "+currYMD.d+" "+getMonthStr(currYMD.m)); //weeknum+
+		var fromDateStr = currYMD.d+"-"+currYMD.m+"-"+currYMD.y;
+		var toDateStr = (currYMD.d+6)+"-"+currYMD.m+"-"+currYMD.y;
+		//c!onsole.log(fromDateStr +" " + toDateStr);
+		periods.push(new Period(fromDateStr, toDateStr));
+		focusDate.advanceDays(7); //the ++ for the week in Time Object.
+	}
+	// for(var i=0+1; i<numWeeks+1; ++i){ 
+	// 	var weeknum = (currWeek+i)%numWeeks;
+	// 	weeknum = (weeknum==0)? numWeeks : weeknum;
+	// 	var currYMD = getTimeYMD(focusDate);
+	// 	labels.push("Week "+weeknum+": "+currYMD.d+" "+getMonthStr(currYMD.m));
+	// 	focusDate.advanceDays(7); //the ++ for the week in Time Object.
+	// }
+	return {"labels":labels, "periods":periods}
+}
+function Period(fromDateStr, toDateStr){
+	return {"from_date":fromDateStr, "to_date":toDateStr};
+}
+
+
+	
+	
+
 	var lineChartData = {
 		labels : ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5"],
 		datasets : [
@@ -57,75 +157,7 @@ function plotChart(){
 			// }
 		]
 		
-	}
-
-	for (var i=0; i<datePeriods.length; ++i){
-		//var getData = $.post("python", );
-	}
-	//var myLine = new Chart(chartSpending.getContext("2d")).Line(lineChartData);
-
-}
-function getChartDates(periodInterval){
-  switch (true){
-  case /^month$/.test(periodInterval):
-    break;
-  case /^week$/.test(periodInterval):
-    return getChartWeekLabels(); break;
-  }
-}
-function updateChartPlot(chartCanvas, lineChartData){
-	var myLine = new Chart(chartCanvas.getContext("2d")).Line(lineChartData);
-}
-
-
-function Period(fromDateStr, toDateStr){
-	return {"from_date":fromDateStr, "to_date":toDateStr};
-}
-
-function getChartWeekLabels(){ //function weekLabels(){ // weekLabels(currTimeObj, currWeek)
-	var currTimeObj = new Time();
-	var currWeek = currTimeObj.weekOfCurrentMonth();
-
-	var YMD = getTimeYMD(currTimeObj);
-	var focusDate = new Time(YMD.y, YMD.m, YMD.d);
-	var numWeeksThisMonth = focusDate.weeksInMonth();
-
-	var numWeeks = 0;
-	var firstDisplayWeek = null;
-	//If not the last week of the month, need to show weeks from previous month.
-	if (currWeek<numWeeksThisMonth){
-		focusDate.advanceMonths(-1);
-		//Don't get .weeksInMonth() from .advanceMonths(-1).beginningOfWeek()
-		//Bad example: Today is Friday, 1 Nov. One month back is Tuesday, 1 Oct.
-		//First day of week would be Monday, 30 Sept. Number of weeks in that first day, would belong to Sept, not Oct.
-		numWeeks = focusDate.weeksInMonth(); //numWeeksLastMonth
-		focusDate.beginningOfWeek();
-		firstDisplayWeek = focusDate;
-	}
-	else {
-		focusDate.beginningOfMonth();
-		numWeeks = numWeeksThisMonth;
-		focusDate.beginningOfWeek();
-		firstDisplayWeek = focusDate;
-	}
-
-	var labels = [];
-	var periods = [];
-	for(var i=0+1; i<numWeeks+1; ++i){ 
-		var weeknum = (currWeek+i)%numWeeks;
-		weeknum = (weeknum==0)? numWeeks : weeknum;
-		var currYMD = getTimeYMD(focusDate);
-		labels.push("Week "+weeknum+": "+currYMD.d+" "+getMonthStr(currYMD.m));
-		focusDate.advanceDays(7); //the ++ for the week in Time Object.
-	}
-	return {"labels":labels, "periods":""}
-}
-
-
-	
-	
-
-
+	};
 
 
 
