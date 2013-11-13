0395f94fb694021e1c2ecccc9af123142988af7f
cl <cl@startup.org.sg>
2013-11-13 22:26:37 +0800

    get spending

--- src/backend/bigspoon/bigspoon/assets/js/chart/chartmanager.js
+++ src/backend/bigspoon/bigspoon/assets/js/chart/chartmanager.js
@@ -1,3 +1,16 @@
+(function(){
+//The host and STAFF_API_URLS is a duplicate from main.js -- cannot access encapsulated variables in mainjs
+var host = "http://"+location.host;
+var STAFF_API_URLS = {
+    "req": host+"/api/v1/ackreq",
+    "bill": host+"/api/v1/closebill",
+    "order": host+"/api/v1/ackorder",
+    "note": host+"/api/v1/note",
+    "dish": host+"/api/v1/dish",
+    "spending": host+"/api/v1/spending"
+}
+
+
 var chartdata = [];
 
 if (dataIsNotLoaded()){
@@ -55,9 +68,17 @@ function createChart(){
 var testData = [1,2,3,65,59,90,81,56,55,40];
 datePeriods = testData;
 	for (var i=0; i<datePeriods.length; ++i){
-		//var getData = $.post("python", );
-		chartData.push(testData[i]);
-		updateChart(chartSpending, lineChartData);
+        $.post(
+            STAFF_API_URLS["spending"], //this requires main.js
+            datePeriods[i]
+        ).done(function(data) {
+			var spending = getSpending(data);
+			chartData.push(spending);
+			updateChart(chartSpending, lineChartData);
+        }).fail(function(data) {
+            console.log("POST failed");
+            console.log(data);
+        });
 	}
 	//var myLine = new Chart(chartSpending.getContext("2d")).Line(lineChartData);
 
@@ -75,7 +96,7 @@ function updateChart(chartCanvas, lineChartData){
 }
 
 
-
+//--------------------------------------------------
 function getChartWeekLabels(){ //function weekLabels(){ // weekLabels(currTimeObj, currWeek)
 	var currTimeObj = new Time();
 	var numWeeksThisMonth = currTimeObj.weeksInMonth();
@@ -133,9 +154,15 @@ function Period(fromDateStr, toDateStr){
 	return {"from_date":fromDateStr, "to_date":toDateStr};
 }
 
-
-	
-	
+//--------------------------------------------------
+function getSpending(meal_data){
+	//nest-level controller
+	var spending = 0.0;
+	for(var i=0; i<meal_data.length; ++i){
+		spending+= meal_data[i].spending;
+	}
+	return spending;
+}
 
 	var lineChartData = {
 		labels : ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5"],
@@ -174,5 +201,7 @@ function chartIsNotCreated(){
 }
 
 
+
+})(); //end of script encapsulation
 
 
