2a1f3b63e1f3abb7fb8add767ddb4df82a4397ba
Qiao Liang <qiaoliang89@gmail.com>
2013-12-22 02:52:07 +0800

    added highlighting row and loading indicator in outlet view

--- src/diner/BigSpoonDiner/BigSpoonDiner/OutletsTableViewController.m
+++ src/diner/BigSpoonDiner/BigSpoonDiner/OutletsTableViewController.m
@@ -14,6 +14,7 @@
     CLLocationManager* locationManager;
     NSString* messageFromMenuPage;
     NSDictionary *jsonForMenuView;
+    UIActivityIndicatorView *indicator;
 }
 
 @end
@@ -76,6 +77,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self initActivityIndicator];
     NSUserDefaults* userDefault = [NSUserDefaults standardUserDefaults];
     if(![userDefault boolForKey:KEY_FOR_SHOW_TUT_DEFAULT]){
         [self showIntroWithCustomView];
@@ -94,13 +96,25 @@
     
     
     [TestFlight passCheckpoint:@"CheckPoint:User Checking Outlets list"];
-    
+    self.outletsTableView.allowsSelection = YES;
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
+
+
+- (void)initActivityIndicator
+{
+    indicator = [[UIActivityIndicatorView alloc]initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    indicator.frame = CGRectMake(0.0, 0.0, 40.0, 40.0);
+    indicator.center = self.view.center;
+    [self.view addSubview:indicator];
+    [indicator bringSubviewToFront:self.view];
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = TRUE;
+}
+
 
 - (void) viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
@@ -391,12 +405,13 @@
     Outlet *outlet = [self.outletsArray objectAtIndex:indexPath.row];
     
     // Deselect the row. Otherwise it will remain being selected.
-    [tableView deselectRowAtIndexPath:indexPath animated:NO];
+    //[tableView deselectRowAtIndexPath:indexPath animated:NO];
     
     if (outlet.isActive) {
         NSLog(@"Is Active haha!!");
         NSLog(@"Row: %d, ID: %d", indexPath.row, outlet.outletID);
         [[User sharedInstance] loadDishesAndTableInfosFromServerForOutlet: outlet.outletID];
+        [indicator startAnimating];
     } else{
         UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@""
                                                             message:@"The restaurant is coming soon"
@@ -408,6 +423,7 @@
 }
 
 - (void) moveToMenuView: (NSNotification*) notif{
+    [indicator stopAnimating];
     jsonForMenuView = (NSDictionary* )[notif object];
     [self performSegueWithIdentifier:@"SegueFromOutletsToMenu" sender:self];
 }
