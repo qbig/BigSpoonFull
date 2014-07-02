3c7fb1908274b78ef52f074064abcac5dac3f7c0
Qiao Liang <qiaoliang89@gmail.com>
2014-07-02 14:55:23 +0800

    try photo edit

--- src/backend/bigspoon/bigspoon/assets/js/main.js
+++ src/backend/bigspoon/bigspoon/assets/js/main.js
@@ -416,7 +416,7 @@ $(document).ready(function() {
             "is_active":is_active,
             "photo": photo,
             "outlet": outlet_ids[0],
-            "categories":[category_id]
+            "categories":JSON.stringify([category_id])
         };
 console.log(req_data);
         $.post(
