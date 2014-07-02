197eb5253b6c4e23e465adb05f188a3bc328c3f1
Qiao Liang <qiaoliang89@gmail.com>
2014-07-02 17:45:01 +0800

    try photo edit

--- src/backend/bigspoon/bigspoon/bg_api/views.py
+++ src/backend/bigspoon/bigspoon/bg_api/views.py
@@ -763,30 +763,33 @@ class UpdateDish(generics.GenericAPIView):
     authentication_classes = (SessionAuthentication,)
     permission_classes = (IsAuthenticated,)
     model = Dish
-    parser_classes = (parsers.FileUploadParser,)
 
     def post(self, req, *args, **kwargs):
         id = int(kwargs['pk'])
-        # serializer = DishSerializer(data=req.DATA, files=req.FILES)
-        # if serializer.is_valid():
-        #     serializer.save()
-        #     return Response(serializer.data)
-        # else :
-        #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         try:
             dish = Dish.objects.get(id=id)
         except Dish.DoesNotExist:
             raise Http404
-        dish.name = req.DATA['name']
-        dish.price = Decimal(str(req.DATA['price']))
-        dish.pos = req.DATA['pos']
-        dish.desc = req.DATA['desc']
-        dish.start_time = req.DATA['start_time']
-        dish.end_time = req.DATA['end_time']
-        dish.quantity = int(req.DATA['quantity'])
-        dish.is_active = bool(req.DATA['is_active'])
-        dish.save()
-        return Response(req.FILES.keys(), status=status.HTTP_200_OK)
+        serializer = DishSerializer(dish, data=req.DATA, files=req.FILES)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else :
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # try:
+        #     dish = Dish.objects.get(id=id)
+        # except Dish.DoesNotExist:
+        #     raise Http404
+        # dish.name = req.DATA['name']
+        # dish.price = Decimal(str(req.DATA['price']))
+        # dish.pos = req.DATA['pos']
+        # dish.desc = req.DATA['desc']
+        # dish.start_time = req.DATA['start_time']
+        # dish.end_time = req.DATA['end_time']
+        # dish.quantity = int(req.DATA['quantity'])
+        # dish.is_active = bool(req.DATA['is_active'])
+        # dish.save()
+        # return Response(req.FILES.keys(), status=status.HTTP_200_OK)
         # return Response(DishSerializer(dish).data,
         #                 status=status.HTTP_200_OK)
 
