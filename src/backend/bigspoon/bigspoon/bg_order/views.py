407dc79eafb1bdfb05ece135a0a453f1136c3a76
Qiao Liang <qiaoliang89@gmail.com>
2014-10-16 12:30:17 +0800

    add natural sort for table listing

--- src/backend/bigspoon/bigspoon/bg_order/views.py
+++ src/backend/bigspoon/bigspoon/bg_order/views.py
@@ -98,8 +98,8 @@ class TableView(ListView):
                               'meals__diner__meals',
                               'meals', 'meals__orders')\
             .filter(outlet__in=outlets).all()
-        return tables
-        #return sorted(tables, key=lambda tb : natural_sort_key(tb.name))
+        #return tables
+        return sorted(tables, key=lambda tb : natural_sort_key(tb.name))
 
     def get_context_data(self, **kwargs):
         context = super(TableView, self).get_context_data(**kwargs)
