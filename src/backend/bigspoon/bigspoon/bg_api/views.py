95f3db72e9802a3003cff95cdb40be216c9de3e5
Qiao Liang <qiaoliang89@gmail.com>
2014-12-11 18:56:31 +0800

    amax integration

--- src/backend/bigspoon/bigspoon/bg_api/views.py
+++ src/backend/bigspoon/bigspoon/bg_api/views.py
@@ -432,8 +432,8 @@ class CreateMeal(generics.CreateAPIView, generics.RetrieveAPIView):
             # send to printer if configured
         
             if get_printing_task(table.outlet.id):
-                #get_printing_task(table.outlet.id).delay(table.outlet, table, new_order)
-                get_printing_task(table.outlet.id)(table.outlet, table, new_order)
+                get_printing_task(table.outlet.id).delay(table.outlet, table, new_order)
+                #get_printing_task(table.outlet.id)(table.outlet, table, new_order)
                 
             # send done
             dish.quantity -= quantity
