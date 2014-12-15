4b3f28c9c46c9bcce85464c570003322614caf2f
Qiao Liang <qiaoliang89@gmail.com>
2014-12-16 01:36:16 +0800

    use celery for socketio call

--- src/backend/bigspoon/bigspoon/bg_api/tasks.py
+++ src/backend/bigspoon/bigspoon/bg_api/tasks.py
@@ -4,9 +4,9 @@ from celery import task
 from utils import send_socketio_message, send_user_feedback
 
 @task
-def send_socketio_message_aync(chan_list, message_data):
+def send_socketio_message_async(chan_list, message_data):
 	send_socketio_message(chan_list, message_data)
 
 @task
-def send_user_feedback_aync(user, message_data):
+def send_user_feedback_async(user, message_data):
 	send_user_feedback(user, message_data)
